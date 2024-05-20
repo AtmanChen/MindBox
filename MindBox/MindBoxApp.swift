3e6f8f0255509dddb2c3e16eadbbfe529d806b5a
Lambert <123@example.com>
2024-05-20 15:25:44 +0800

    project init

---
+++ MindBox/MindBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindBoxApp.swift
+//  MindBox
+//
+//  Created by lambert on 2024/5/20.
+//
+
+import SwiftUI
+
+@main
+struct MindBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
